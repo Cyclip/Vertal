921969daa531339c71133630e4a4df3cd824e4a7
Cyclip <40525455+Cyclip@users.noreply.github.com>
2021-07-03 15:38:55 +0100

    Add Djongo
    Use Djongo to implement MongoDB

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,12 +78,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'server',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+                'host': os.getenv("MONGO_CONNECTION_STRING"),
+        }
+    }
+}
 
 
 # Password validation
